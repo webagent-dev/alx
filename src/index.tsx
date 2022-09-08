2e172e541365e9444890af4034ce6a18e7f63d14
webagent-dev <wizkenny019@gmail.com>
2022-09-08 03:27:28 +0100

    webpack and babel configs

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDom from 'react-dom/client'
+import App from './App'
+const Root = ReactDom.createRoot(document.getElementById('root') as HTMLElement)
+
+Root.render(<App />)
